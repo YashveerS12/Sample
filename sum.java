ee8789e3a95d95dc32d92d1c5e2ace1f39ebd77b
Yashveer Singh <harshitsingh00010@gmail.com>
2024-06-06 00:05:15 +0530

    added message in sum

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 public class sum{
     public static void main(String args[])
     {
-        System.out.println(" Hello I am Yashveer");
+        System.out.println(" Hello I am Yashveer Singh");
     }
 }
